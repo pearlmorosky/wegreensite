32b733cfd4d56c2f56d96d1e2425478134c4d556
e-soto-02 <52751053+e-soto-02@users.noreply.github.com>
2019-07-24 15:43:50 -0500

    Add files via upload

---
+++ calendarfinalproject.js
@@ -0,0 +1,381 @@
+
+document.getElementById("day 1").addEventListener("mouseover", myFunction1);
+
+function myFunction1() {
+document.getElementById("day 1").innerHTML = "Try to keep your trash from the day in a jar";
+}
+document.getElementById("day 1").addEventListener("mouseout", myFunction1num);
+
+function myFunction1num() {
+document.getElementById("day 1").innerHTML = "1";
+}
+
+
+
+document.getElementById("day 2").addEventListener("mouseover", myFunction2);
+
+function myFunction2() {
+document.getElementById("day 2").innerHTML = "Wash all laundry on cold";
+}
+
+document.getElementById("day 2").addEventListener("mouseout", myFunction2num);
+
+function myFunction2num() {
+document.getElementById("day 2").innerHTML = "2";
+}
+
+
+
+document.getElementById("day 3").addEventListener("mouseover", myFunction3);
+
+function myFunction3() {
+document.getElementById("day 3").innerHTML = "Take a shower in less than 10 min";
+}
+document.getElementById("day 3").addEventListener("mouseout", myFunction3num);
+
+function myFunction3num() {
+document.getElementById("day 3").innerHTML = "3";
+}
+
+
+
+document.getElementById("day 4").addEventListener("mouseover", myFunction4);
+
+function myFunction4() {
+document.getElementById("day 4").innerHTML = "Use only reusable shopping bags today";
+}
+document.getElementById("day 4").addEventListener("mouseout", myFunction4num);
+
+function myFunction4num() {
+document.getElementById("day 4").innerHTML = "4";
+}
+
+
+
+document.getElementById("day 5").addEventListener("mouseover", myFunction5);
+
+function myFunction5() {
+document.getElementById("day 5").innerHTML = "Get a houseplant, put eggshells on it";
+}
+document.getElementById("day 5").addEventListener("mouseout", myFunction5num);
+
+function myFunction5num() {
+document.getElementById("day 5").innerHTML = "5";
+}
+
+
+
+document.getElementById("day 6").addEventListener("mouseover", myFunction6);
+
+function myFunction6() {
+document.getElementById("day 6").innerHTML = "Use reusable napkins";
+}
+document.getElementById("day 6").addEventListener("mouseout", myFunction6num);
+
+function myFunction6num() {
+document.getElementById("day 6").innerHTML = "6";
+}
+
+
+
+document.getElementById("day 7").addEventListener("mouseover", myFunction7);
+
+function myFunction7() {
+document.getElementById("day 7").innerHTML = "Use only reusable cutlery today";
+}
+document.getElementById("day 7").addEventListener("mouseout", myFunction7num);
+
+function myFunction7num() {
+document.getElementById("day 7").innerHTML = "7";
+}
+
+
+
+document.getElementById("day 8").addEventListener("mouseover", myFunction8);
+
+function myFunction8() {
+document.getElementById("day 8").innerHTML = "Water plants when dark outside";
+}
+document.getElementById("day 8").addEventListener("mouseout", myFunction8num);
+
+function myFunction8num() {
+document.getElementById("day 8").innerHTML = "8";
+}
+
+
+
+document.getElementById("day 9").addEventListener("mouseover", myFunction9);
+
+function myFunction9() {
+document.getElementById("day 9").innerHTML = "Cook outside";
+}
+document.getElementById("day 9").addEventListener("mouseout", myFunction9num);
+
+function myFunction9num() {
+document.getElementById("day 9").innerHTML = "9";
+}
+
+
+
+document.getElementById("day 10").addEventListener("mouseover", myFunction10);
+
+function myFunction10() {
+document.getElementById("day 10").innerHTML = "Open windows for today";
+}
+document.getElementById("day 10").addEventListener("mouseout", myFunction10num);
+
+function myFunction10num() {
+document.getElementById("day 10").innerHTML = "10";
+}
+
+
+
+document.getElementById("day 11").addEventListener("mouseover", myFunction11);
+
+function myFunction11() {
+document.getElementById("day 11").innerHTML = "Don't use Electronics";
+}
+document.getElementById("day 11").addEventListener("mouseout", myFunction11num);
+
+function myFunction11num() {
+document.getElementById("day 11").innerHTML = "11";
+}
+
+
+
+document.getElementById("day 12").addEventListener("mouseover", myFunction12);
+
+function myFunction12() {
+document.getElementById("day 12").innerHTML = "Go barefaced";
+}
+document.getElementById("day 12").addEventListener("mouseout", myFunction12num);
+
+function myFunction12num() {
+document.getElementById("day 12").innerHTML = "12";
+}
+
+
+
+
+document.getElementById("day 13").addEventListener("mouseover", myFunction13);
+
+function myFunction13() {
+document.getElementById("day 13").innerHTML = "Turn off AC/heater";
+}
+document.getElementById("day 13").addEventListener("mouseout", myFunction13num);
+
+function myFunction13num() {
+document.getElementById("day 13").innerHTML = "13";
+}
+
+
+
+document.getElementById("day 14").addEventListener("mouseover", myFunction14);
+
+function myFunction14() {
+document.getElementById("day 14").innerHTML = "Be frugal with energy usage";
+}
+document.getElementById("day 14").addEventListener("mouseout", myFunction14num);
+
+function myFunction14num() {
+document.getElementById("day 14").innerHTML = "14";
+}
+
+
+
+document.getElementById("day 15").addEventListener("mouseover", myFunction15);
+
+function myFunction15() {
+document.getElementById("day 15").innerHTML = "Don't watch TV";
+}
+document.getElementById("day 15").addEventListener("mouseout", myFunction15num);
+
+function myFunction15num() {
+document.getElementById("day 15").innerHTML = "15";
+}
+
+
+
+document.getElementById("day 16").addEventListener("mouseover", myFunction16);
+
+function myFunction16() {
+document.getElementById("day 16").innerHTML = "Clean sink faucet and plumbing";
+}
+document.getElementById("day 16").addEventListener("mouseout", myFunction16num);
+
+function myFunction16num() {
+document.getElementById("day 16").innerHTML = "16";
+}
+
+
+
+document.getElementById("day 17").addEventListener("mouseover", myFunction17);
+
+function myFunction17() {
+document.getElementById("day 17").innerHTML = "Use a powerstrip turn (on/off)";
+}
+document.getElementById("day 17").addEventListener("mouseout", myFunction17num);
+
+function myFunction17num() {
+document.getElementById("day 17").innerHTML = "17";
+}
+
+
+document.getElementById("day 18").addEventListener("mouseover", myFunction18);
+
+function myFunction18() {
+document.getElementById("day 18").innerHTML = "Dry clothes outside";
+}
+document.getElementById("day 18").addEventListener("mouseout", myFunction18num);
+
+function myFunction18num() {
+document.getElementById("day 18").innerHTML = "18";
+}
+
+
+document.getElementById("day 19").addEventListener("mouseover", myFunction19);
+
+function myFunction19() {
+document.getElementById("day 19").innerHTML = "Don't take a hot shower";
+}
+document.getElementById("day 19").addEventListener("mouseout", myFunction19num);
+
+function myFunction19num() {
+document.getElementById("day 19").innerHTML = "19";
+}
+
+
+
+document.getElementById("day 20").addEventListener("mouseover", myFunction20);
+
+function myFunction20() {
+document.getElementById("day 20").innerHTML = "Air dry dishes";
+}
+document.getElementById("day 20").addEventListener("mouseout", myFunction20num);
+
+function myFunction20num() {
+document.getElementById("day 20").innerHTML = "20";
+}
+
+
+document.getElementById("day 21").addEventListener("mouseover", myFunction21);
+
+function myFunction21() {
+document.getElementById("day 21").innerHTML = "Don't use plastic";
+}
+document.getElementById("day 21").addEventListener("mouseout", myFunction21num);
+
+function myFunction21num() {
+document.getElementById("day 21").innerHTML = "21";
+}
+
+
+
+document.getElementById("day 22").addEventListener("mouseover", myFunction22);
+
+function myFunction22() {
+document.getElementById("day 22").innerHTML = "Wash only a full load (dishes/laundry)";
+}
+document.getElementById("day 22").addEventListener("mouseout", myFunction22num);
+
+function myFunction22num() {
+document.getElementById("day 22").innerHTML = "22";
+}
+
+
+
+document.getElementById("day 23").addEventListener("mouseover", myFunction23);
+
+function myFunction23() {
+document.getElementById("day 23").innerHTML = "Set refrigerator to a warmer temperature";
+}
+document.getElementById("day 23").addEventListener("mouseout", myFunction23num);
+
+function myFunction23num() {
+document.getElementById("day 23").innerHTML = "23";
+}
+
+
+
+document.getElementById("day 24").addEventListener("mouseover", myFunction24);
+
+function myFunction24() {
+document.getElementById("day 24").innerHTML = "Avoid disposable utensils";
+}
+document.getElementById("day 24").addEventListener("mouseout", myFunction24num);
+
+function myFunction24num() {
+document.getElementById("day 24").innerHTML = "24";
+}
+
+
+document.getElementById("day 25").addEventListener("mouseover", myFunction25);
+
+function myFunction25() {
+document.getElementById("day 25").innerHTML = "Have a recycling bins";
+}
+document.getElementById("day 25").addEventListener("mouseout", myFunction25num);
+
+function myFunction25num() {
+document.getElementById("day 25").innerHTML = "25";
+}
+
+
+document.getElementById("day 26").addEventListener("mouseover", myFunction26);
+
+function myFunction26() {
+document.getElementById("day 26").innerHTML = "Don't use the stove";
+}
+document.getElementById("day 26").addEventListener("mouseout", myFunction26num);
+
+function myFunction26num() {
+document.getElementById("day 26").innerHTML = "26";
+}
+
+
+document.getElementById("day 27").addEventListener("mouseover", myFunction27);
+
+function myFunction27() {
+document.getElementById("day 27").innerHTML = "Go vegan for the day ";
+}
+document.getElementById("day 27").addEventListener("mouseout", myFunction27num);
+
+function myFunction27num() {
+document.getElementById("day 27").innerHTML = "27";
+}
+
+
+document.getElementById("day 28").addEventListener("mouseover", myFunction28);
+
+function myFunction28() {
+document.getElementById("day 28").innerHTML = "Use public transportation";
+}
+document.getElementById("day 28").addEventListener("mouseout", myFunction28num);
+
+function myFunction28num() {
+document.getElementById("day 28").innerHTML = "28";
+}
+
+
+document.getElementById("day 29").addEventListener("mouseover", myFunction29);
+
+function myFunction29() {
+document.getElementById("day 29").innerHTML = "Unplug your appliances and such";
+}
+document.getElementById("day 29").addEventListener("mouseout", myFunction29num);
+
+function myFunction29num() {
+document.getElementById("day 29").innerHTML = "29";
+}
+
+
+document.getElementById("day 30").addEventListener("mouseover", myFunction30);
+
+function myFunction30() {
+document.getElementById("day 30").innerHTML = "Drink only water";
+}
+document.getElementById("day 30").addEventListener("mouseout", myFunction30num);
+
+function myFunction30num() {
+document.getElementById("day 30").innerHTML = "30";
+}
